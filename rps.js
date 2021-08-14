d783e175772f9c79ac6e0d9314b7cc3489c1bd55
Justin <justin.crippen@gmail.com>
2021-08-14 14:41:40 -0500

    created new function determineWinner. Tested and working

--- rps.js
+++ rps.js
@@ -23,3 +23,33 @@ const getUserChoice = (userInput) => {
      console.log("I will have to think about this. This question will take some time, 7... million.... years....")
  }
  }
+console.log(getComputerChoice())
+
+const determineWinner = (userChoice, computerChoice) => {
+    if(userChoice === computerChoice) {
+       return 'Game Tied, go again!';
+    }
+    if(userChoice === "rock"){
+        if(computerChoice === "paper") {
+            return "No one understands how paper beats a rock, PC WINS!"
+        } else {
+            return 'Running with scissors is both dangerous and futile. ROCK SMASH! You WIN!!!';
+        }
+    } else if (userChoice === "paper") {
+            if (computerChoice === "scissors") {
+                return "You got cut so bad, you wish you nevah been cut! PC WINS!"
+            } else { 
+                return "Covering up that rock again? You WIN...for now."
+            }
+    } else if (userChoice === "scissors") {
+         if (computerChoice === "rock") {
+         return "Stop getting so smashed!!! Loser!"
+         } else { 
+             return "Yay! you a weeeeener"
+         }
+        }
+    }
+
+console.log (determineWinner("rock", "scissors"))
+   
+    
